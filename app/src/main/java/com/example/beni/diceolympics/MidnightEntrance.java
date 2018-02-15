42e99029c92eb4b90c70d1a408895350be4d1e5f
Benny Katz <bennykatz1012@gmail.com>
2018-02-15 13:49:57 +0200

    -to fix GameFunction.rolldice function

--- app/src/main/java/com/example/beni/diceolympics/MidnightEntrance.java
+++ app/src/main/java/com/example/beni/diceolympics/MidnightEntrance.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MidnightEntrance extends AppCompatActivity {
+
+    String[] getNamesArr;
+    int[] getScores;
+
+    MediaPlayer buttonClickSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +30,10 @@ public class MidnightEntrance extends AppCompatActivity {
         if (Sounds.getIsMute()) btnMute.setImageResource(R.drawable.mute);
         else btnMute.setImageResource(R.drawable.unmute);
 
-        final MediaPlayer buttonClickSound = MediaPlayer.create(MidnightEntrance.this, R.raw.buttonpress);
+        buttonClickSound = MediaPlayer.create(MidnightEntrance.this, R.raw.buttonpress);
 
-        final String[] getNamesArr = getIntent().getStringArrayExtra("NameArr");
-        final int[] getScores = getIntent().getIntArrayExtra("ScoresArr");
+        getNamesArr = getIntent().getStringArrayExtra("NameArr");
+        getScores = getIntent().getIntArrayExtra("ScoresArr");
 
 
         btnStartGame.setOnClickListener(new View.OnClickListener() {
@@ -70,5 +76,22 @@ public class MidnightEntrance extends AppCompatActivity {
                 Sounds.buttonMutePress(btnMute);
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+
+            if (!Sounds.getIsMute()) buttonClickSound.start();
+            final Intent intent = new Intent(MidnightEntrance.this, ChooseGame.class);
+            intent.putExtra("NameArr", getNamesArr);
+            intent.putExtra("ScoresArr", getScores);
+            startActivity(intent);
+            finish();
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
